0f2339936a73a42745481fa232eea4d837fe7dd5
Jared Cleghorn <jaredrcleghorn@gmail.com>
2020-07-11 19:24:02 -0500

    Add Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello, world')
+import express from 'express'
+import config from '../config.json'
+
+const app = express()
+
+app.get('/', (request, response) => response.send('hello, world'))
+
+// Start listening.
+app.listen(config.express.port, () => console.log(`Auto Pet Feeder API listening at http://localhost:${config.express.port}`))
