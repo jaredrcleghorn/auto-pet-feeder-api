eff87807326068f92a0d0aee7c5f5adda6e01a2f
Jared Cleghorn <jaredrcleghorn@gmail.com>
2020-08-15 02:23:08 -0500

    Add Mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import config from '../config.json'
 
 const app = express()
 
 app.get('/', (request, response) => response.send('hello, world'))
 
-// Start listening.
-app.listen(config.express.port, () => console.log(`Auto Pet Feeder API listening at http://localhost:${config.express.port}`))
+// Connect to the database.
+mongoose.connect(`mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.database}`, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+	.then(() => {
+		console.log('Connected to the database')
+
+		// Start listening.
+		app.listen(config.express.port, () => console.log(`Auto Pet Feeder API listening at http://localhost:${config.express.port}`))
+	})
+	.catch(console.error)
