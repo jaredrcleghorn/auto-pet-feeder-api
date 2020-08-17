0e2cd8418bd3f7ef2cad3201aa35548539097c14
Jared Cleghorn <jaredrcleghorn@gmail.com>
2020-08-17 06:28:11 -0500

    Add meal event retrieval

--- src/routers/meals-router.js
+++ src/routers/meals-router.js
@@ -1,16 +1,18 @@
+import EventEmitter from 'events'
 import express, { Router } from 'express'
 import mongoose from 'mongoose'
 import { verifyToken } from '../functions'
 import { Feeder, Meal } from '../models'
 
 const mealsRouter = Router()
+const eventEmitter = new EventEmitter()
 
 mealsRouter.post('/', verifyToken, express.json(), async (request, response) => {
 	const feeder = await Feeder.findById(request.body.feederId).exec()
 
 	if (feeder !== null && feeder.email === response.locals.email) {
 		try {
-			await Meal.create([{
+			const [meal] = await Meal.create([{
 				feederId: request.body.feederId,
 				name: request.body.name,
 				starts: request.body.starts,
@@ -18,6 +20,7 @@ mealsRouter.post('/', verifyToken, express.json(), async (request, response) => 
 				repeat: request.body.repeat,
 			}])
 
+			eventEmitter.emit('event', 'creation', meal._id, request.body.starts, request.body.ms, request.body.repeat)
 			response.sendStatus(201)
 		} catch (error) {
 			if (error instanceof mongoose.Error.ValidationError) {
@@ -39,6 +42,24 @@ mealsRouter.get('/', verifyToken, async (request, response) => {
 		response.json(await Meal.findByFeederId(request.query.feederId))
 	} else {
 		response.status(400).send('Invalid Feeder ID')
+	}
+})
+
+mealsRouter.get('/events', verifyToken, async (request, response) => {
+	const feeder = await Feeder.findById(request.query.feederId).exec()
+
+	if (feeder !== null && feeder.email === response.locals.email) {
+		response.set('Content-Type', 'text/event-stream')
+		response.writeHead(200)
+
+		eventEmitter.on('event', (type, id, starts, ms, repeat) => {
+			response.write(JSON.stringify({
+				type,
+				meal: { id, starts, ms, repeat },
+			}))
+		})
+	} else {
+		response.sendStatus(400).send('Invalid Feeder ID')
 	}
 })
 
